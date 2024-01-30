1f61ebcd573f9d512ee0400472e53a95ae7e6cd3
Karolina O. G. Borges <karol.ogb.ti@gmail.com>
2024-01-29 23:00:27 -0300

    First connit infra

---
+++ cadastro-de-alunos-infra/src/main/java/com/cadastrodealunos/cadastrodealunosinfra/CadastroDeAlunosInfraApplication.java
@@ -0,0 +1,13 @@
+package com.cadastrodealunos.cadastrodealunosinfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeAlunosInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeAlunosInfraApplication.class, args);
+	}
+
+}
---
+++ cadastro-de-alunos-infra/src/test/java/com/cadastrodealunos/cadastrodealunosinfra/CadastroDeAlunosInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastrodealunos.cadastrodealunosinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeAlunosInfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
